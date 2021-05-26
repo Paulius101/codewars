e15eebb6245f110f1aca56ba1a6659cfd11c01db
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-05-26 22:11:12 +0300

    task++

--- __tests__/bartenderDrinks.js
+++ __tests__/bartenderDrinks.js
@@ -1,8 +1,25 @@
 function getDrinkByProfession(param) {
     var convertedName;
+    var formated = param[0].toUpperCase();
+    var formated1 = formated.slice(1).toLowerCase();
     switch (param) {
-        case 'jabrOni':
+        case 'Jabroni':
             convertedName = `Patron Trquila`;
+            break;
+        case 'School Counselor':
+            convertedName = `Anything with Alcohol`;
+            break;
+        case 'Programmer':
+            convertedName = `Hipster Craft Beer`;
+            break;
+        case 'Bike Gang Member':
+            convertedName = `Moonshine`;
+            break;
+        case 'Politician':
+            convertedName = `Your tax dollars`;
+            break;
+        case 'Rapper':
+            convertedName = `Cristal`;
             break;
 
         default:
